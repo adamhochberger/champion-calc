6089076be45af96734782216cdcfddb17fb34843
Adam Hochberger <adamhochberger@gmail.com>
2019-03-02 10:24:28 -0500

    added comments and automatic version check to query file

--- query.py
+++ query.py
@@ -1,6 +1,22 @@
 import requests
 import json
 
-r = requests.get("http://ddragon.leagueoflegends.com/cdn/9.4.1/data/en_US/champion/Azir.json")
+# Initial call to api to check most recent version listed in the file listing (LIFO system results in it being at the first position)
+versionCheck = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
+recentVersion = json.loads(versionCheck.text)[0]
+
+# Print statement to verify the current version it is on
+print(recentVersion)
+
+# Secondary api call to the overall champion file that uses most recent version to pull information
+r = requests.get("http://ddragon.leagueoflegends.com/cdn/" + recentVersion + "/data/en_US/champion.json")
 data = json.loads(r.text)
-print(json.dumps(data["data"]["Azir"]["spells"], indent=4))
+
+# Test statement to verify that champion names are being pulled properly
+for key in list(data["data"]):
+    print(key)
+
+# Names of all champions can be pulled using 
+# data = json.loads(r.text) 
+# list(data["data"])
+# where r is a request made to the champion.json file
