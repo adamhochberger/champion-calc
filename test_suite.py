ddc7951800cdc739a874997b9b7e093b1eecef5e
Adam Hochberger <adamhochberger@gmail.com>
2020-07-13 16:36:58 -0400

    Change testing to reflect model functionality difference

--- test_suite.py
+++ test_suite.py
@@ -10,10 +10,10 @@ class Test_TestComponentInit:
         assert self.v.scaling == {"value": [15, 40, 65, 90, 115]}
 
     def test_component_rank(self):
-        assert self.v.comp_at_rank(5) == 115
+        assert self.v.scaling["value"][4] == 115
 
     def test_component_dict(self):
         assert Damage_Heal("Physical", self.v.scaling).scaling == {"kind": "Physical", 
-        "base": self.v.scaling, "ad_%": {}, "ap_%": {}, "hp_%": {}}
+        "base": self.v.scaling, "ad_%": Component().scaling, "ap_%": Component().scaling, "hp_%": Component().scaling}
 
     
